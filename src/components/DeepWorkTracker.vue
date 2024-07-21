0d8980f856252b976a80716cecd01caeafccbaa9
Thomas Hestenes Jakobsen <Thomas.Hestenes.Jakobsen@ae.no>
2024-07-21 21:24:33 +0200

    Added pinia store

--- src/components/DeepWorkTracker.vue
+++ src/components/DeepWorkTracker.vue
@@ -1,138 +1,146 @@
 <template>
-    <div>
-      <button @click="showAddModal = true" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-6 w-full">
-        Add Deep Work...
-      </button>
-  
-      <AddDeepWorkModal v-if="showAddModal" :editWork="editingWork" @close="closeModal" @save="saveWork" />
-  
-      <ConfirmationModal 
-        v-if="showConfirmationModal"
-        :title="confirmationModalData.title"
-        :message="confirmationModalData.message"
-        :confirmText="confirmationModalData.confirmText"
-        :cancelText="confirmationModalData.cancelText"
-        @confirm="confirmationModalData.onConfirm"
-        @cancel="closeConfirmationModal"
-      />
-  
-      <DeepWorkItem 
-        v-for="work in store.works" 
-        :key="work.id" 
-        :work="work"
-        @edit="editWork"
-        @toggleComplete="toggleComplete"
-        @reset="confirmReset"
-        @delete="confirmDelete"
-        @toggle="toggleWork"
-      />
-    </div>
-  </template>
-  
-  <script>
-  import { ref, reactive } from 'vue'
-  import { useDeepWorkStore } from '../store/deepWork'
-  import AddDeepWorkModal from './AddDeepWorkModal.vue'
-  import DeepWorkItem from './DeepWorkItem.vue'
-  import ConfirmationModal from './ConfirmationModal.vue'
-  
-  export default {
-    name: 'DeepWorkTracker',
-    components: {
-      AddDeepWorkModal,
-      DeepWorkItem,
-      ConfirmationModal
-    },
-    setup() {
-      const showAddModal = ref(false)
-      const showConfirmationModal = ref(false)
-      const store = useDeepWorkStore()
-      const editingWork = ref(null)
-      const confirmationModalData = reactive({
-        title: '',
-        message: '',
-        confirmText: 'Confirm',
-        cancelText: 'Cancel',
-        onConfirm: () => {}
-      })
-  
-      const closeModal = () => {
-        showAddModal.value = false
-        editingWork.value = null
+  <div>
+    <button @click="showAddModal = true" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-6 w-full">
+      Add Deep Work...
+    </button>
+
+    <AddDeepWorkModal v-if="showAddModal" :editWork="editingWork" @close="closeModal" @save="saveWork" />
+
+    <ConfirmationModal 
+      v-if="showConfirmationModal"
+      :title="confirmationModalData.title"
+      :message="confirmationModalData.message"
+      :confirmText="confirmationModalData.confirmText"
+      :cancelText="confirmationModalData.cancelText"
+      @confirm="confirmationModalData.onConfirm"
+      @cancel="closeConfirmationModal"
+    />
+
+    <DeepWorkItem 
+      v-for="work in store.works" 
+      :key="work.id" 
+      :work="work"
+      @edit="editWork"
+      @toggleComplete="toggleComplete"
+      @reset="confirmReset"
+      @delete="confirmDelete"
+      @toggle="toggleWork"
+    />
+  </div>
+</template>
+
+<script>
+import { ref, reactive, onMounted, onUnmounted } from 'vue'
+import { useDeepWorkStore, initializeStore } from '../store/deepWork'
+import AddDeepWorkModal from './AddDeepWorkModal.vue'
+import DeepWorkItem from './DeepWorkItem.vue'
+import ConfirmationModal from './ConfirmationModal.vue'
+
+export default {
+  name: 'DeepWorkTracker',
+  components: {
+    AddDeepWorkModal,
+    DeepWorkItem,
+    ConfirmationModal
+  },
+  setup() {
+    const showAddModal = ref(false)
+    const showConfirmationModal = ref(false)
+    const store = useDeepWorkStore()
+    const editingWork = ref(null)
+    const confirmationModalData = reactive({
+      title: '',
+      message: '',
+      confirmText: 'Confirm',
+      cancelText: 'Cancel',
+      onConfirm: () => {}
+    })
+
+    onMounted(() => {
+      initializeStore()
+    })
+
+    onUnmounted(() => {
+      store.stopTimeTracking()
+    })
+
+    const closeModal = () => {
+      showAddModal.value = false
+      editingWork.value = null
+    }
+
+    const saveWork = (workData) => {
+      if (editingWork.value) {
+        store.updateWork(editingWork.value.id, workData)
+      } else {
+        store.addDeepWork(workData)
       }
-  
-      const saveWork = (workData) => {
-        if (editingWork.value) {
-          store.updateWork(editingWork.value.id, workData)
-        } else {
-          store.addDeepWork(workData)
-        }
-        closeModal()
-      }
-  
-      const editWork = (work) => {
-        editingWork.value = work
-        showAddModal.value = true
-      }
-  
-      const toggleComplete = (work) => {
-        store.toggleComplete(work.id)
-      }
-  
-      const confirmReset = (work) => {
-        showConfirmationModal.value = true
-        confirmationModalData.title = 'Reset Deep Work'
-        confirmationModalData.message = `Are you sure you want to reset "${work.name}"? This will set the time spent back to 0.`
-        confirmationModalData.onConfirm = () => {
-          store.resetWork(work.id)
-          closeConfirmationModal()
-        }
-      }
-  
-      const confirmDelete = (work) => {
-        showConfirmationModal.value = true
-        confirmationModalData.title = 'Delete Deep Work'
-        confirmationModalData.message = `Are you sure you want to delete "${work.name}"? This action cannot be undone.`
-        confirmationModalData.onConfirm = () => {
-          store.deleteWork(work.id)
-          closeConfirmationModal()
-        }
-      }
-  
-      const toggleWork = (work) => {
-        const activeWork = store.works.find(w => w.isActive && w.id !== work.id)
-        if (activeWork) {
-          showConfirmationModal.value = true
-          confirmationModalData.title = 'Switch Active Deep Work'
-          confirmationModalData.message = `Do you want to pause "${activeWork.name}" and start "${work.name}"?`
-          confirmationModalData.onConfirm = () => {
-            store.toggleWork(work.id)
-            closeConfirmationModal()
-          }
-        } else {
-          store.toggleWork(work.id)
-        }
-      }
-  
-      const closeConfirmationModal = () => {
-        showConfirmationModal.value = false
-      }
-  
-      return {
-        showAddModal,
-        showConfirmationModal,
-        store,
-        editingWork,
-        confirmationModalData,
-        closeModal,
-        saveWork,
-        editWork,
-        toggleComplete,
-        confirmReset,
-        confirmDelete,
-        toggleWork,
-        closeConfirmationModal
+      closeModal()
+    }
+
+    const editWork = (work) => {
+      editingWork.value = work
+      showAddModal.value = true
+    }
+
+    const toggleComplete = (work) => {
+      store.toggleComplete(work.id)
+    }
+
+    const confirmReset = (work) => {
+      showConfirmationModal.value = true
+      confirmationModalData.title = 'Reset Deep Work'
+      confirmationModalData.message = `Are you sure you want to reset "${work.name}"? This will set the time spent back to 0.`
+      confirmationModalData.onConfirm = () => {
+        store.resetWork(work.id)
+        closeConfirmationModal()
       }
     }
+
+    const confirmDelete = (work) => {
+      showConfirmationModal.value = true
+      confirmationModalData.title = 'Delete Deep Work'
+      confirmationModalData.message = `Are you sure you want to delete "${work.name}"? This action cannot be undone.`
+      confirmationModalData.onConfirm = () => {
+        store.deleteWork(work.id)
+        closeConfirmationModal()
+      }
+    }
+
+    const toggleWork = (work) => {
+      const activeWork = store.works.find(w => w.isActive && w.id !== work.id)
+      if (activeWork) {
+        showConfirmationModal.value = true
+        confirmationModalData.title = 'Switch Active Deep Work'
+        confirmationModalData.message = `Do you want to pause "${activeWork.name}" and start "${work.name}"?`
+        confirmationModalData.onConfirm = () => {
+          store.toggleWork(work.id)
+          closeConfirmationModal()
+        }
+      } else {
+        store.toggleWork(work.id)
+      }
+    }
+
+    const closeConfirmationModal = () => {
+      showConfirmationModal.value = false
+    }
+
+    return {
+      showAddModal,
+      showConfirmationModal,
+      store,
+      editingWork,
+      confirmationModalData,
+      closeModal,
+      saveWork,
+      editWork,
+      toggleComplete,
+      confirmReset,
+      confirmDelete,
+      toggleWork,
+      closeConfirmationModal
+    }
   }
-  </script>
+}
+</script>
--- src/store/deepWork.js
+++ src/store/deepWork.js
@@ -1,146 +1,158 @@
-import { reactive } from 'vue'
+import { defineStore } from 'pinia'
 
-const state = reactive({
-  works: [],
-  lastUpdateTime: Date.now(),
+export const useDeepWorkStore = defineStore('deepWork', {
+  state: () => ({
+    works: [],
+    lastUpdateTime: Date.now(),
+    intervalId: null,
+  }),
 
-  addDeepWork(newWork) {
-    this.works.push({
-      id: Date.now(),
-      name: newWork.name,
-      description: newWork.description,
-      hours: newWork.hours,
-      timeSpent: 0,
-      isActive: false,
-      isCompleted: false,
-      startTime: null,
-    })
-    this.saveToLocalStorage()
-  },
-
-  updateWork(id, updatedWork) {
-    const index = this.works.findIndex(w => w.id === id)
-    if (index !== -1) {
-      this.works[index] = { ...this.works[index], ...updatedWork }
+  actions: {
+    addDeepWork(newWork) {
+      this.works.push({
+        id: Date.now(),
+        name: newWork.name,
+        description: newWork.description,
+        hours: newWork.hours,
+        timeSpent: 0,
+        isActive: false,
+        isCompleted: false,
+        startTime: null,
+      })
       this.saveToLocalStorage()
-    }
-  },
+    },
 
-  toggleComplete(id) {
-    const work = this.works.find(w => w.id === id)
-    if (work) {
-      work.isCompleted = !work.isCompleted
-      if (work.isCompleted) {
-        work.isActive = false
-        work.startTime = null
+    updateWork(id, updatedWork) {
+      const index = this.works.findIndex(w => w.id === id)
+      if (index !== -1) {
+        this.works[index] = { ...this.works[index], ...updatedWork }
+        this.saveToLocalStorage()
       }
-      this.saveToLocalStorage()
-    }
-  },
+    },
 
-  resetWork(id) {
-    const work = this.works.find(w => w.id === id)
-    if (work) {
-      work.timeSpent = 0
-      work.isCompleted = false
-      work.isActive = false
-      work.startTime = null
-      this.saveToLocalStorage()
-    }
-  },
-
-  deleteWork(id) {
-    const index = this.works.findIndex(w => w.id === id)
-    if (index !== -1) {
-      this.works.splice(index, 1)
-      this.saveToLocalStorage()
-    }
-  },
-
-  toggleWork(id) {
-    const work = this.works.find(w => w.id === id)
-    if (work) {
-      if (work.isActive) {
-        // Pause the work
-        work.isActive = false
-        work.timeSpent += (Date.now() - work.startTime) / 1000
-        work.startTime = null
-      } else {
-        // Start the work
-        // First, pause any other active work
-        this.works.forEach(w => {
-          if (w.isActive) {
-            w.isActive = false
-            w.timeSpent += (Date.now() - w.startTime) / 1000
-            w.startTime = null
-          }
-        })
-        work.isActive = true
-        work.startTime = Date.now()
-      }
-      this.saveToLocalStorage()
-    }
-  },
-
-  updateActiveWorks() {
-    const now = Date.now()
-    const timeDiff = (now - this.lastUpdateTime) / 1000
-    this.lastUpdateTime = now
-
-    this.works.forEach(work => {
-      if (work.isActive) {
-        work.timeSpent += timeDiff
-        if (work.timeSpent >= work.hours * 3600) {
-          work.isCompleted = true
+    toggleComplete(id) {
+      const work = this.works.find(w => w.id === id)
+      if (work) {
+        work.isCompleted = !work.isCompleted
+        if (work.isCompleted) {
           work.isActive = false
           work.startTime = null
         }
+        this.saveToLocalStorage()
       }
-    })
+    },
 
-    this.saveToLocalStorage()
+    resetWork(id) {
+      const work = this.works.find(w => w.id === id)
+      if (work) {
+        work.timeSpent = 0
+        work.isCompleted = false
+        work.isActive = false
+        work.startTime = null
+        this.saveToLocalStorage()
+      }
+    },
+
+    deleteWork(id) {
+      const index = this.works.findIndex(w => w.id === id)
+      if (index !== -1) {
+        this.works.splice(index, 1)
+        this.saveToLocalStorage()
+      }
+    },
+
+    toggleWork(id) {
+      const work = this.works.find(w => w.id === id)
+      if (work) {
+        if (work.isActive) {
+          // Pause the work
+          work.isActive = false
+          work.timeSpent += (Date.now() - work.startTime) / 1000
+          work.startTime = null
+        } else {
+          // Start the work
+          // First, pause any other active work
+          this.works.forEach(w => {
+            if (w.isActive) {
+              w.isActive = false
+              w.timeSpent += (Date.now() - w.startTime) / 1000
+              w.startTime = null
+            }
+          })
+          work.isActive = true
+          work.startTime = Date.now()
+        }
+        this.saveToLocalStorage()
+      }
+    },
+
+    updateActiveWorks() {
+      const now = Date.now()
+      const timeDiff = (now - this.lastUpdateTime) / 1000
+      this.lastUpdateTime = now
+
+      this.works.forEach(work => {
+        if (work.isActive) {
+          work.timeSpent += timeDiff
+          if (work.timeSpent >= work.hours * 3600) {
+            work.isCompleted = true
+            work.isActive = false
+            work.startTime = null
+          }
+        }
+      })
+
+      this.saveToLocalStorage()
+    },
+
+    saveToLocalStorage() {
+      localStorage.setItem('deepWorks', JSON.stringify(this.works))
+      localStorage.setItem('lastUpdateTime', this.lastUpdateTime.toString())
+    },
+
+    loadFromLocalStorage() {
+      const savedWorks = localStorage.getItem('deepWorks')
+      const savedLastUpdateTime = localStorage.getItem('lastUpdateTime')
+      if (savedWorks) {
+        this.works = JSON.parse(savedWorks)
+      }
+      if (savedLastUpdateTime) {
+        this.lastUpdateTime = parseInt(savedLastUpdateTime, 10)
+      }
+      this.updateActiveWorks() // Update times on load
+    },
+
+    startTimeTracking() {
+      if (typeof window !== 'undefined') {
+        // Browser environment
+        this.intervalId = setInterval(() => this.updateActiveWorks(), 1000)
+
+        // Handle visibility change
+        document.addEventListener('visibilitychange', () => {
+          if (document.hidden) {
+            // Clear the interval when the tab is not visible
+            clearInterval(this.intervalId)
+          } else {
+            // When the tab becomes visible again, update times and restart the interval
+            this.updateActiveWorks()
+            this.intervalId = setInterval(() => this.updateActiveWorks(), 1000)
+          }
+        })
+      }
+    },
+
+    stopTimeTracking() {
+      if (this.intervalId) {
+        clearInterval(this.intervalId)
+      }
+    },
   },
-
-  saveToLocalStorage() {
-    localStorage.setItem('deepWorks', JSON.stringify(this.works))
-    localStorage.setItem('lastUpdateTime', this.lastUpdateTime.toString())
-  },
-
-  loadFromLocalStorage() {
-    const savedWorks = localStorage.getItem('deepWorks')
-    const savedLastUpdateTime = localStorage.getItem('lastUpdateTime')
-    if (savedWorks) {
-      this.works = JSON.parse(savedWorks)
-    }
-    if (savedLastUpdateTime) {
-      this.lastUpdateTime = parseInt(savedLastUpdateTime, 10)
-    }
-    this.updateActiveWorks() // Update times on load
-  }
 })
 
-state.loadFromLocalStorage()
-
-// Set up interval to update time spent on active works
-const updateInterval = 1000 // 1 second
-
-if (typeof window !== 'undefined') {
-  // Browser environment
-  let intervalId = setInterval(() => state.updateActiveWorks(), updateInterval)
-
-  // Handle visibility change
-  document.addEventListener('visibilitychange', () => {
-    if (document.hidden) {
-      // Clear the interval when the tab is not visible
-      clearInterval(intervalId)
-    } else {
-      // When the tab becomes visible again, update times and restart the interval
-      state.updateActiveWorks()
-      intervalId = setInterval(() => state.updateActiveWorks(), updateInterval)
-    }
-  })
-}
-
-export function useDeepWorkStore() {
-  return state
+// Initialize the store
+export function initializeStore() {
+  const store = useDeepWorkStore()
+  store.loadFromLocalStorage()
+  store.startTimeTracking()
+  return store
 }
